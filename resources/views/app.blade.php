4232c69a89cf363102a80b7818a14f4567fb53ff
hector yegre <programadoryegre@gmail.com>
2023-08-02 13:13:02 -0400

    cambios para prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html data-bs-theme="dark" lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	@vite('resources/js/app.js')
 	@inertiaHead
